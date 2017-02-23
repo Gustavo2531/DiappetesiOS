faf91524a914d8de52136d5b60378bedef062557
Gustavo Méndez <gustavomendez@MacBook-Pro-de-Gustavo.local>
2017-02-23 12:24:34 -0600

    Mapas

--- ParseStarterProject/MapViewController.swift
+++ ParseStarterProject/MapViewController.swift
@@ -18,34 +18,113 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     override func viewDidLoad() {
         
             
-            mapView.delegate = self
-            mapView.showsUserLocation = true
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.longpress(gestureRecognizer:)))
+        
+        uilpgr.minimumPressDuration = 2
+        
+        mapView.addGestureRecognizer(uilpgr)
+        
             locationManager.delegate = self
-            
-            //Check for Location Services
-            if (CLLocationManager.locationServicesEnabled()) {
-                locationManager = CLLocationManager()
-                locationManager.delegate = self
-                locationManager.desiredAccuracy = kCLLocationAccuracyBest
-                locationManager.requestAlwaysAuthorization()
-                locationManager.requestWhenInUseAuthorization()
-            }
+           locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.requestWhenInUseAuthorization()
-            if CLLocationManager.locationServicesEnabled() {
-                locationManager.startUpdatingLocation()
-            }
-            //Zoom to user location
-            let noLocation = CLLocationCoordinate2D()
-            let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 200, 200)
-            mapView.setRegion(viewRegion, animated: false)
+            locationManager.startUpdatingLocation()
+        
+        
+            // Get place details to display on map
             
-            DispatchQueue.main.async {
-                self.locationManager.startUpdatingLocation()
-        }
+        
+                
+        
+                            
+        
+        
     }
-    
-    
+
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            
+            let touchPoint = gestureRecognizer.location(in: self.mapView)
+            
+            let newCoordinate = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+            
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            var title = ""
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                
+                if error != nil {
+                    
+                    print(error)
+                    
+                } else {
+                    
+                    if let placemark = placemarks?[0] {
+                        
+                        if placemark.subThoroughfare != nil {
+                            
+                            title += placemark.subThoroughfare! + " "
+                            
+                        }
+                        
+                        if placemark.thoroughfare != nil {
+                            
+                            title += placemark.thoroughfare!
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+                if title == "" {
+                    
+                    title = "Added \(NSDate())"
+                    
+                }
+                
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = newCoordinate
+                
+                annotation.title = title
+                
+                self.mapView.addAnnotation(annotation)
+                
+               
+                
+                
+            })
+            
+            
+        }
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let latitude = location.latitude
+        let longitude = location.longitude
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+      
+        
+        self.mapView.setRegion(region, animated: true)
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        self.mapView.addAnnotation(annotation)
+
+        
+        self.mapView.setRegion(region, animated: true)
+        
+    }
+
     @IBAction func returnMenu(_ sender: Any) {
         let transition: CATransition = CATransition()
         transition.duration = 0.5
