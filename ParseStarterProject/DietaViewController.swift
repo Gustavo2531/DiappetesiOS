573e7f82e38788fa93447c6c224f087356a06e1f
Gustavo Méndez <gustavomendez@MacBook-Pro-de-Gustavo.local>
2017-03-16 10:56:03 -0600

    Alimentación Ya esta, solo falta desgloce

--- ParseStarterProject/DietaViewController.swift
+++ ParseStarterProject/DietaViewController.swift
@@ -7,15 +7,89 @@
 //
 
 import UIKit
-
+import Parse
 class DietaViewController: UIViewController {
-
+    
+    var firstDinner = ""
+    var secondDinner = ""
+    var thirdDinner = ""
+    var firstBreakfast = ""
+    var secondBreakFast = ""
+    var thirdBreakFast = ""
+    var firstMeal = ""
+    var secondMeal = ""
+    var thirdMeal = ""
+    
+    @IBOutlet weak var firstBreakfastLabel: UILabel!
+    @IBOutlet weak var secondBreakfastLabel: UILabel!
+    @IBOutlet weak var thirdBreakfastlabel: UILabel!
+    @IBOutlet weak var firstMealLabel: UILabel!
+    @IBOutlet weak var secondMealLabel: UILabel!
+    @IBOutlet weak var thirdMealLabel: UILabel!
+    @IBOutlet weak var firstDinnerLabel: UILabel!
+    @IBOutlet weak var secondDinnerLabel: UILabel!
+    @IBOutlet weak var thirdDinnerLabel: UILabel!
+    
+    func obtainDatos(){
+        let query = PFQuery(className:"Dieta")
+        query.whereKey("userId", equalTo: (PFUser.current()?.objectId)!)
+        query.order(byDescending: "createdAt");
+        query.getFirstObjectInBackground(block: {(object, error)
+            in
+            if error != nil{
+                self.firstBreakfastLabel.text=self.firstBreakfast
+                self.secondBreakfastLabel.text = self.secondBreakFast
+                self.thirdBreakfastlabel.text=self.thirdBreakFast
+                self.firstMealLabel.text=self.firstMeal
+                self.secondMealLabel.text=self.secondMeal
+                self.thirdMealLabel.text=self.thirdMeal
+                self.thirdDinnerLabel.text=self.thirdDinner
+                self.firstDinnerLabel.text=self.firstDinner
+                self.secondDinnerLabel.text=self.secondDinner
+                print(error!)
+            }else{
+                
+                self.firstDinner=((object?["firstDinner"] as AnyObject).description)
+                
+                self.secondDinner=((object?["secondDinner"] as AnyObject).description)
+                
+                self.thirdDinner=((object?["thirdDinner"] as AnyObject).description)
+                
+                self.firstBreakfast=((object?["firstBreakfast"] as AnyObject).description)
+                
+                self.secondBreakFast=((object?["secondBreakFast"] as AnyObject).description)
+                
+                self.thirdBreakFast=((object?["thirdBreakFast"] as AnyObject).description)
+                
+                self.firstMeal=((object?["firstMeal"] as AnyObject).description)
+                
+                self.secondMeal=((object?["secondMeal"] as AnyObject).description)
+                
+                self.thirdMeal=((object?["thirdMeal"] as AnyObject).description)
+                
+                self.firstBreakfastLabel.text=self.firstBreakfast
+                self.secondBreakfastLabel.text = self.secondBreakFast
+                self.thirdBreakfastlabel.text=self.thirdBreakFast
+                self.firstMealLabel.text=self.firstMeal
+                self.secondMealLabel.text=self.secondMeal
+                self.thirdMealLabel.text=self.thirdMeal
+                self.thirdDinnerLabel.text=self.thirdDinner
+                self.firstDinnerLabel.text=self.firstDinner
+                self.secondDinnerLabel.text=self.secondDinner
+            }
+            
+        })
+    }
     override func viewDidLoad() {
+        obtainDatos()
+
         super.viewDidLoad()
         self.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.red], for:.selected)
+        print(self.firstDinner)
+       
         // Do any additional setup after loading the view.
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- ParseStarterProject/GraficasViewController.swift
+++ ParseStarterProject/GraficasViewController.swift
@@ -7,14 +7,107 @@
 //
 
 import UIKit
-
+import Parse
 class GraficasViewController: UIViewController {
-
+var caloriasSemanal=0.0
+var caloriasHoy=0.0
+var caloriasMensual=0.0
+    
+    
+    @IBOutlet weak var caloriasHoyLabel: UILabel!
+    @IBOutlet weak var caloriasSemanalLabel: UILabel!
+    @IBOutlet weak var caloriasMonthLabel: UILabel!
+    
     override func viewDidLoad() {
+        obtainDatos()
         super.viewDidLoad()
         self.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.red], for:.selected)
 
         // Do any additional setup after loading the view.
+    }
+    func obtainDatos(){
+        var calendar = NSCalendar.autoupdatingCurrent
+        calendar.timeZone = NSTimeZone(forSecondsFromGMT: 0) as TimeZone
+        
+        let today = Date()
+        let cal = Calendar(identifier: .gregorian)
+        let newDate = cal.startOfDay(for: today)
+        
+        
+        
+        let tommorow = Calendar.current.date(byAdding: .day, value: 1, to: newDate)
+    
+        let query = PFQuery(className:"Food")
+        query.whereKey("userId", equalTo: (PFUser.current()?.objectId)!)
+        query.order(byDescending: "createdAt");
+       
+        query.whereKey("createdAt", greaterThanOrEqualTo: newDate)
+        query.whereKey("createdAt", lessThan: tommorow!)
+        
+        query.findObjectsInBackground(block: { (objects, error) in
+            if error != nil{
+                print(error!)
+            }else{
+                for object in objects!{
+                    
+                    self.caloriasHoy=self.caloriasHoy+(object["quantity"] as! Double)
+                    
+                }
+                self.caloriasHoyLabel.text=String(self.caloriasHoy)
+                
+            }
+            
+        })
+        
+        let week = Calendar.current.date(byAdding: .weekday, value: 1, to: newDate)
+        
+        let query2 = PFQuery(className:"Food")
+        query2.whereKey("userId", equalTo: (PFUser.current()?.objectId)!)
+        query2.order(byDescending: "createdAt");
+        
+        query2.whereKey("createdAt", greaterThanOrEqualTo: newDate)
+        query2.whereKey("createdAt", lessThan: week!)
+        
+        query2.findObjectsInBackground(block: { (objects, error) in
+            if error != nil{
+                print(error!)
+            }else{
+                for object in objects!{
+                    
+                    self.caloriasSemanal=self.caloriasSemanal+(object["quantity"] as! Double)
+                    
+                }
+                self.caloriasSemanalLabel.text=String(self.caloriasSemanal)
+            }
+            
+        })
+        
+        let month = Calendar.current.date(byAdding: .month, value: 1, to: newDate)
+        
+        let query3 = PFQuery(className:"Food")
+        query3.whereKey("userId", equalTo: (PFUser.current()?.objectId)!)
+        query3.order(byDescending: "createdAt");
+        
+        query3.whereKey("createdAt", greaterThanOrEqualTo: newDate)
+        query3.whereKey("createdAt", lessThan: month!)
+        
+        query3.findObjectsInBackground(block: { (objects, error) in
+            if error != nil{
+                print(error!)
+            }else{
+                for object in objects!{
+                    
+                    self.caloriasMensual=self.caloriasMensual+(object["quantity"] as! Double)
+                    
+                }
+                self.caloriasMonthLabel.text=String(self.caloriasMensual)
+                
+            }
+            
+        })
+        
+        
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
