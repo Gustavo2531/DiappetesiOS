89a9f0fb4183811215d5adbac7651280cd218185
Gustavo Méndez <gustavomendez@MacBook-Pro-de-Gustavo.local>
2017-03-20 12:38:02 -0600

    Medicamentos Listos, Solo faltan gráficas, se podrían poner recordatorios, pero lo dejamos para el tercer parcial

--- ParseStarterProject/MedicamentosViewController.swift
+++ ParseStarterProject/MedicamentosViewController.swift
@@ -7,13 +7,40 @@
 //
 
 import UIKit
-
+import Parse
 class MedicamentosViewController: UIViewController {
+    @IBOutlet weak var medicamento: UILabel!
+    @IBOutlet weak var medicamento2: UILabel!
+    @IBOutlet weak var medicamento3: UILabel!
+    @IBOutlet weak var medicamento4: UILabel!
+    @IBOutlet weak var medicamento5: UILabel!
 
+    @IBOutlet weak var tiempo: UILabel!
+    @IBOutlet weak var tiempo2: UILabel!
+    @IBOutlet weak var tiempo3: UILabel!
+    @IBOutlet weak var tiempo4: UILabel!
+    @IBOutlet weak var tiempo5: UILabel!
+    
+    var tiempoString = ""
+    var tiempoString2 = ""
+    var tiempoString3 = ""
+    var tiempoString4 = ""
+    var tiempoString5 = ""
+    
     override func viewDidLoad() {
+        self.medicamento.text=""
+        self.medicamento2.text=""
+        self.medicamento3.text=""
+        self.medicamento4.text=""
+        self.medicamento5.text=""
+        self.tiempo.text=""
+        self.tiempo2.text=""
+        self.tiempo3.text=""
+        self.tiempo4.text=""
+        self.tiempo5.text=""
         super.viewDidLoad()
         self.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.red], for:.selected)
-
+        obtainDatos()
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -23,6 +50,58 @@ class MedicamentosViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func obtainDatos(){
+        let query = PFQuery(className:"Recetas")
+        query.whereKey("userId", equalTo: (PFUser.current()?.objectId)!)
+        query.order(byDescending: "createdAt");
+        query.getFirstObjectInBackground(block: {(object, error)
+            in
+            if error != nil{
+                self.medicamento.text=""
+                self.medicamento2.text=""
+                self.medicamento3.text=""
+                self.medicamento4.text=""
+                self.medicamento5.text=""
+                self.tiempo.text=""
+                self.tiempo2.text=""
+                self.tiempo3.text=""
+                self.tiempo4.text=""
+                self.tiempo5.text=""
+                print(error!)
+            }else{
+                
+                
+                if ((object?["Medicamento4"] as AnyObject).description) != "" {
+                    self.tiempoString4 = "Cada "+((object?["HoraMedicamento4"] as AnyObject).description)+" Hora por "+((object?["DiaMedicamento4"] as AnyObject).description)+" dia/s"
+                }
+                if ((object?["Medicamento2"] as AnyObject).description) != "" {
+                    self.tiempoString2 = "Cada "+((object?["HoraMedicamento2"] as AnyObject).description)+" Hora por "+((object?["DiaMedicamento2"] as AnyObject).description)+" dia/s"
+                }
+                if ((object?["Medicamento3"] as AnyObject).description) != "" {
+                    self.tiempoString3 = "Cada "+((object?["HoraMedicamento3"] as AnyObject).description)+" Hora por "+((object?["DiaMedicamento3"] as AnyObject).description)+" dia/s"
+                }
+                if ((object?["Medicamento5"] as AnyObject).description) != "" {
+                    self.tiempoString5 = "Cada "+((object?["HoraMedicamento5"] as AnyObject).description)+" Hora por "+((object?["DiaMedicamento5"] as AnyObject).description)+" dia/s"
+                }
+                if ((object?["Medicamento1"] as AnyObject).description) != "" {
+                    self.tiempoString = "Cada "+((object?["HoraMedicamento1"] as AnyObject).description)+" Hora por "+((object?["DiaMedicamento1"] as AnyObject).description)+" dia/s"
+                }
+                
+                self.medicamento.text=((object?["Medicamento1"] as AnyObject).description)+" mg"
+                self.medicamento2.text=((object?["Medicamento2"] as AnyObject).description)+" mg"
+                self.medicamento3.text=((object?["Medicamento3"] as AnyObject).description)+" mg"
+                self.medicamento4.text=((object?["Medicamento4"] as AnyObject).description)+" mg"
+                self.medicamento5.text=((object?["Medicamento5"] as AnyObject).description)+" mg"
+                self.tiempo.text=self.tiempoString
+                self.tiempo2.text=self.tiempoString2
+                self.tiempo3.text=self.tiempoString3
+                self.tiempo4.text=self.tiempoString4
+                self.tiempo5.text=self.tiempoString5
+            }
+            
+        })
     }
     
     @IBAction func returnMenu(_ sender: Any) {
