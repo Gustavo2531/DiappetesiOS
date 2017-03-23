e70d963980fbcfce02a9dce8ddfa96fe4c8ae581
Gustavo Méndez <gustavomendez@MacBook-Pro-de-Gustavo.local>
2017-03-23 14:39:24 -0600

    WatchKit

---
+++ WatchDiappetes Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchDiappetes Extension
+//
+//  Created by Gustavo Méndez on 23/03/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
