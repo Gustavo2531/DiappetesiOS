29ce479fbfa7bae10e71a4cf28a5383f5ac00703
Gustavo Méndez <gustavomendez@MacBook-Pro-de-Gustavo.local>
2017-03-29 21:00:47 -0600

    Solo falta la imagen del watch

---
+++ WatchDiappetes Extension/CalendarioInterfaceController.swift
@@ -0,0 +1,82 @@
+//
+//  CalendarioInterfaceController.swift
+//  Diappetes
+//
+//  Created by Héctor Carlos Flores Reynoso on 3/23/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import EventKit
+
+class CalendarioInterfaceController: WKInterfaceController {
+    
+    var rows: [String] = []
+
+    @IBOutlet var table: WKInterfaceTable!
+ 
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        let eventStore = EKEventStore()
+        
+        eventStore.requestAccess(to: EKEntityType.event, completion:
+            {(granted, error) in
+                if !granted {
+                    print("Access to store not granted")
+                }
+        })
+        
+        let calendars = eventStore.calendars(for: EKEntityType.event)
+        
+        for calendar in calendars as [EKCalendar] {
+            if (calendar.title == "Calendar"){
+               
+                let dateFormatter = DateFormatter()
+                
+                var date=Date()
+                
+                let format=DateFormatter()
+                format.dateFormat = "dd-MM-yyyy HH:mm"
+                
+                dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+                let oldDate = dateFormatter.date(from: format.string(from: date))
+                
+                
+                date.addTimeInterval(5*60*60*24)
+
+                dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+                let newDate = dateFormatter.date(from: format.string(from: date))
+                
+                let predicates = eventStore.predicateForEvents(withStart: oldDate!, end: newDate!, calendars: [calendar])
+                let eventos=eventStore.events(matching: predicates)
+                
+                
+                for (_, item) in eventos.enumerated() {
+                    puts("\(item.title)")
+                    rows.append("\(item.title)")
+                }
+                
+            }
+        }
+        
+        table.setNumberOfRows(rows.count, withRowType: "MainRowType")
+        for (i,_) in rows.enumerated(){
+            let row=table.rowController(at: i) as! MyRowController
+            row.itemLabel.setText(rows[i])
+        }
+        
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ WatchDiappetes Extension/MyRowController.swift
@@ -0,0 +1,16 @@
+//
+//  MyRowController.swift
+//  Diappetes
+//
+//  Created by Héctor Carlos Flores Reynoso on 3/29/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import WatchKit
+
+class MyRowController: NSObject {
+
+    
+    @IBOutlet var itemLabel: WKInterfaceLabel!
+    
+}
