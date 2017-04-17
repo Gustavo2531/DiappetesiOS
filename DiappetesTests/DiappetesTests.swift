601c745f07d052ad6f3726fe0cc025f9f14d15f6
Gustavo Méndez <gustavomendez@MacBook-Pro-de-Gustavo.local>
2017-04-17 15:29:03 -0500

    Commit all

---
+++ DiappetesTests/DiappetesTests.swift
@@ -0,0 +1,41 @@
+//
+//  DiappetesTests.swift
+//  DiappetesTests
+//
+//  Created by Gustavo Méndez on 17/04/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import XCTest
+
+class DiappetesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPorDos(){
+//        let num=2
+//        let resultado=Prueba()
+//        
+//        XCTAssertEqual(resultado.porDos(num), 4)
+    }
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
